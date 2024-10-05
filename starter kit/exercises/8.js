fa36faf01c99771df7d4ad0dfe41c3db15bf95d9
staff227 <wnfynn@gmail.com>
2024-10-05 17:40:40 +0000

    animation

---
+++ starter kit/exercises/8.js
@@ -0,0 +1,7 @@
+const loaderContainer = document.querySelector(".loaderContainer")
+const loader = document.querySelector(".loader")
+const bg = document.querySelector(".bg")
+
+window.addEventListener("load", ()=>{
+    loaderContainer.classList.add("working")
+})
